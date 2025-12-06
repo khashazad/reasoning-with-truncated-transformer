38f2999a659e335883ce8e1be3ac271b733331d2
lullu57 <kezitogaleacurmi@gmail.com>
2025-12-06 16:46:26 -0500

    major reorganisation of final experiment

--- truncate-models.sh
+++
@@ -1,8 +0,0 @@
-HF_REPO_ID="khashazad/Qwen2.5-Math-1.5B-truncated"  # replace with your repo id
-
-python truncate_models.py \
-  --models Qwen/Qwen2.5-Math-1.5B \
-  --layer_index_start 20 \
-  --keep_last_layer \
-  --output_dir reasoning-with-sampling/models \
-  --device cpu
